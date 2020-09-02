e09bbd5018ab63b6987963c4c0421d4790bfaca0
Andrey <petukhovanvk@gmail.com>
2020-09-02 10:45:17 +0300

    реализован функционал на React

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './components/app/';
 
-class WhoAmI extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            years: 26,
-            nationality: 'uk',
-        }
-        this.nextYear = this.nextYear.bind(this);
-    }
-    nextYear() {
-        this.setState(state => ({
-            years: ++state.years
-        }))
-    }
+// class WhoAmI extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             years: 26,
+//             nationality: 'uk',
+//         }
+//         this.nextYear = this.nextYear.bind(this);
+//     }
+//     nextYear() {
+//         this.setState(state => ({
+//             years: ++state.years
+//         }))
+//     }
 
-    render() {
-        const {name, surname, link} = this.props;
-        const {years} = this.state;
-        return(
-            <>
-                <button onClick={this.nextYear}>++</button>
-                <h1>My name is {name}, surname - {surname}, years - {years}</h1>
-                <a href={link}>My profile</a>
-            </>
-        )
-    }
-}
+//     render() {
+//         const {name, surname, link} = this.props;
+//         const {years} = this.state;
+//         return(
+//             <>
+//                 <button onClick={this.nextYear}>++</button>
+//                 <h1>My name is {name}, surname - {surname}, years - {years}</h1>
+//                 <a href={link}>My profile</a>
+//             </>
+//         )
+//     }
+// }
 
 // function WhoAmI(props) {
 //     return(
@@ -40,15 +42,15 @@ class WhoAmI extends Component {
 //     )
 // }
 
-const All = () => {
-    return (
-        <>
-            <WhoAmI name='John' surname='Smith' link='google.com'/>
-            <WhoAmI name='Ivan' surname='Smith' link='vk.com'/>
-            <WhoAmI name='Alex' surname='Bit' link='yahooo.com'/>
-        </>
-    )
-}
+// const All = () => {
+//     return (
+//         <>
+//             <WhoAmI name='John' surname='Smith' link='google.com'/>
+//             <WhoAmI name='Ivan' surname='Smith' link='vk.com'/>
+//             <WhoAmI name='Alex' surname='Bit' link='yahooo.com'/>
+//         </>
+//     )
+// }
 
 ReactDOM.render(<App/>,  document.getElementById('root'));
 //ReactDOM.render(<All/>,  document.getElementById('root'));
